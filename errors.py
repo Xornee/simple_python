effe38979c1f4acb2ccb473d5146cbd3b1bb37ce
Szymon Mytych <58708377+Xornee@users.noreply.github.com>
2024-10-14 18:05:54 +0200

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,75 @@
+
+# --------- 1 ---- 
+
+# def wypisz_tablice(tablica):
+#     for element in tablica:
+#         print(element)
+
+# tablica = [1, 2, 3, 4, 5]
+# wypisz_tablice(tablica)
+
+
+# -------- 2 ------ 
+
+# def drukuj_zmienna():
+#     print(wartosc)
+
+# drukuj_zmienna()
+
+# -------- 3 -----
+
+# def dodaj(a, b):
+#     return a + b
+
+# wynik = dodaj(a)
+# print(wynik)
+
+# -------- 4 -------
+
+# tablica = [1, 2, 3]
+# print(tablica[2])
+
+
+# -------- 5 -------
+
+
+# def drukuj_wartosci():
+
+#     for i in range(5):
+#         wartosc = i * 2
+#     print(wartosc)
+
+# drukuj_wartosci()
+
+# -------- 6 -------
+
+# def dodaj_do_napisu(napis, liczba):
+    
+#     return int(napis) + liczba
+
+# wynik = dodaj_do_napisu("4", 5)
+# print(wynik)
+
+# -------- 7 -------
+
+# for i in range(5):
+#     if i == 3:
+#         print(i)
+
+# -------- 8 -------
+
+
+# -------- 9 -----
+# licznik = 0
+
+# def zwieksz_licznik(licznikPrzePortal):
+#     licznikPrzePortal += 1
+#     return licznikPrzePortal
+
+# print(zwieksz_licznik(licznik))
+
+
+#### min() / max()
+#### sum() / len()
+#### sorted() / reversed()
+#### tupe() 
